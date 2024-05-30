43b2befcfb9da1506318075ec15604756dcf62fa
Gonzalo Lavin <gonzalolavin99@gmail.com>
2024-05-30 00:55:53 -0400

    UPDATE: Actualización  del header

--- web/src/components/common-layout/index.js
+++ web/src/components/common-layout/index.js
@@ -1,8 +1,10 @@
+import Header from "../header"
+
 function CommonLayout({children}){
     return <div className="mx-auto max-w-7xl p-6 lg:px-8">
         {/* HeaderComponent*/}
-        Header
         {/* HeaderComponent*/}
+        <Header/>
 
         {/*Main  Content*/}
         <main > {children}</main>
--- web/src/components/header/index.js
+++ web/src/components/header/index.js
@@ -1,10 +1,34 @@
-'use client'
+"use client";
+import { Button } from "../ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { AlignJustify, Link } from "lucide-react";
 
-function Header(){
-return 
-<div>
+
+function Header() {
+
     
-</div>
+  return (
+    <div>
+      <header className="flex h-16 w-full shrink-0 items-center">
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button>
+              <AlignJustify className="h-6 w-6" />
+              <span className="sr-only">Toggle Navigation Menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <Link className="mr-6 hidden lg:flex" href={'#'}>
+            <h3>JOBSCO</h3>
+            </Link>
+            <div className="grid gap-2 py-6">
+
+            </div>
+          </SheetContent>
+        </Sheet>
+      </header>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
